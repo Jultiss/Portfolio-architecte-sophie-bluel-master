c79d3ad9c19fdc7fb64fbca7bd79c41fc043fb69
Julien <julien.tissidre@gmail.com>
2023-05-04 09:57:50 +0200

    Filtres JS OK

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,10 +1,11 @@
+// Works import from API //
 let loadedData = [];
 
 async function loadWorks() {
   const response = await fetch("http://localhost:5678/api/works");
   loadedData = await response.json();
   displayWork(loadedData);
-};
+}
 
 function displayWork(data) {
   const galleryEl = document.querySelector(".gallery");
@@ -14,6 +15,40 @@ function displayWork(data) {
       <figcaption>${item.title}</figcaption>
     </figure>
   `).join('');
-};
+}
 
 loadWorks();
+
+// Category import from API //
+
+async function loadCategory() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const loadedData = await response.json();
+  displayCategory(loadedData);
+}
+
+// filter works //
+
+const filterWorks = document.querySelector("#filter-works-all");
+const filterWorksObjets = document.querySelector("#filter-works-Object");
+const filterWorksAppartements = document.querySelector("#filter-works-Appartements");
+const filterWorksHR = document.querySelector("#filter-works-HR");
+
+filterWorks.addEventListener("click", function () {
+  displayWork(loadedData);
+});
+
+filterWorksObjets.addEventListener("click", function() {
+  const filteredData = loadedData.filter(item => item.category.id === 1); 
+  displayWork(filteredData);
+});
+
+filterWorksAppartements.addEventListener("click", function() {
+  const filteredData = loadedData.filter(item => item.category.id === 2);
+  displayWork(filteredData);
+});
+
+filterWorksHR.addEventListener("click", function() {
+  const filteredData = loadedData.filter(item => item.category.id === 3);
+  displayWork(filteredData);
+});
